bc6d7a3ea82b978d7a09c5d39e75fbe91565343c
Kaustuv Karki <karkikaustuv@gmail.com>
2023-05-31 12:43:12 +0545

    basic server side implemented

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
-        <div className="bg-blue-50 h-screen w-screen">
-            <form action="">
-                <input type="text" placeholder="Username" className="block" />
+        <div className="bg-blue-50 h-screen w-screen flex items-center">
+            <form action="" className="w-64 mx-auto mb-20">
                 <input
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    type="text"
+                    placeholder="Username"
+                    className="block w-full rounded-sm p-2 mb-2 h-10 border outline-0"
+                />
+                <input
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     type="password"
                     placeholder="Password"
-                    className="block"
+                    className="block w-full rounded-sm p-2 mb-2 h-10 border outline-0"
                 />
+                <button className="bg-blue-500 text-white block w-full rounded-sm p-2">
+                    Register
+                </button>
             </form>
         </div>
     );
